978eb35e358707b192ac2f0c7f749b41dfe5deb8
YH-edenbro <hyun96227@gmail.com>
2025-07-21 22:58:13 +0900

    투두리스트 Create

--- section08/src/components/Editor.jsx
+++ section08/src/components/Editor.jsx
@@ -1,10 +1,40 @@
 import './Editor.css'
+import { useState, useRef } from 'react'
 
-const Editor = () => {
+
+const Editor = ({ onCreate }) => {
+
+  const [content, setContent] = useState("")
+  const contentRef = useRef()
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value)
+
+  }
+
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      onsubmit()
+    }
+  }
+  
+  const onsubmit = () => {
+    if(content === ""){
+      contentRef.current.focus()
+      return
+    }
+    onCreate(content)
+    setContent("")
+  }
+
   return (
     <div className="Editor">
-      <input placeholder="새로운 Todo..."/>
-      <button>추가</button>
+      <input ref={contentRef}
+      value={content} 
+      onChange={onChangeContent}
+      onKeyDown={onKeyDown} 
+      placeholder="새로운 Todo..."/>
+      <button onClick={onsubmit}>추가</button>
     </div>
   )
 }
